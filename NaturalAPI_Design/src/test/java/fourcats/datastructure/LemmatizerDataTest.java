2bcbf4b2f77d4b32e002f86d79ce0159c3492d87
Andrea Polo <polo.andrea98@gmail.com>
2020-04-12 23:40:29 +0200

    aggiunti primi test

---
+++ NaturalAPI_Design/src/test/java/fourcats/datastructure/LemmatizerDataTest.java
@@ -0,0 +1,36 @@
+package fourcats.datastructure;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LemmatizerDataTest {
+
+    LemmatizerData ld;
+
+    @Before
+    public void before() {
+        ld = new LemmatizerData();
+    }
+
+    @Test
+    public void getList() {
+        assertTrue(ld.getList()!=null);
+    }
+
+    @Test
+    public void addElement() {
+        ld.addElement("dogs","NN","dog");
+        assertEquals(1,ld.getList().size());
+    }
+
+    @Test
+    public void testToString() {
+        ld.addElement("dogs","NN","dog");
+        ld.addElement("went","VBP","go");
+        String expected = "-- Lemmatize Data -- \n[dogs,NN,dog]\n[went,VBP,go]\n";
+        assertEquals(expected,ld.toString());
+    }
+}
---
+++ NaturalAPI_Design/src/test/java/fourcats/datastructure/WordTagTest.java
@@ -0,0 +1,21 @@
+package fourcats.datastructure;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WordTagTest {
+
+    WordTag w;
+
+    @Before
+    public void before() {
+        w = new WordTag("dogs","NN","dog");
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("[dogs,NN,dog]",w.toString());
+    }
+}
