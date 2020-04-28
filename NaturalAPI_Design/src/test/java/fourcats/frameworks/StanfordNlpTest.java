cbc9dcadb708e18f84c3bc524c09b2056192d27a
matteo-munari <matteo.munari2@gmail.com>
2020-04-28 11:53:27 +0200

    Aggiunti test StanfordNlp, AnalyzedData da Discover

---
+++ NaturalAPI_Design/src/test/java/fourcats/frameworks/StanfordNlpTest.java
@@ -0,0 +1,40 @@
+package fourcats.frameworks;
+
+import fourcats.datastructure.AnalyzedData;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StanfordNlpTest {
+
+    StanfordNlp nlp = new StanfordNlp();
+
+    @Test
+    public void testParseDocumentTokenization() {
+        String content = "My dog also likes eating sausage.";
+
+        AnalyzedData data = nlp.parseDocumentContent(content);
+
+        assertEquals("dog",data.getTaggedData().get(1).getLemma());
+        assertEquals("NN",data.getTaggedData().get(1).getTag());
+    }
+
+    @Test
+    public void testParseDocumentDependencies() {
+        String content = "I want to withdraw money from the ATM.";
+
+        AnalyzedData data = nlp.parseDocumentContent(content);
+
+        assertEquals("dobj",data.getDependenciesList().get(4).getRelation());
+    }
+
+    @Test
+    public void testParseNoContent() {
+        String content = "";
+
+        AnalyzedData data = nlp.parseDocumentContent(content);
+
+        assertEquals(0,data.getDependenciesList().size());
+        assertEquals(0,data.getTaggedData().size());
+    }
+}
