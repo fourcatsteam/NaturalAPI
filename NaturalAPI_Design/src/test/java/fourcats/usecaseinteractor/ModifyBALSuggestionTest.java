6a31ffe132f65eb382e661575f3036326eaf4edd
Giulio <giulio.umbrella@gmail.com>
2020-05-04 17:29:50 +0200

    Add modifyBALSuggestionTest

---
+++ NaturalAPI_Design/src/test/java/fourcats/usecaseinteractor/ModifyBALSuggestionTest.java
@@ -0,0 +1,102 @@
+package fourcats.usecaseinteractor;
+
+import fourcats.interfaceaccess.RepositoryAccess;
+import fourcats.port.ModifyBALSuggestionOutputPort;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ModifyBALSuggestionTest {
+
+    @Mock
+    RepositoryAccess repo;
+
+    @Mock
+    ModifyBALSuggestionOutputPort out;
+
+    @InjectMocks
+    ModifyBALSuggestion modifyBALSuggestion;
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testModifyActionType(){
+
+        modifyBALSuggestion.modifyActionType(1,1, "tipo");
+        verify(repo).updateActionType(1,1,"tipo");
+        verify(out).showModifiedActionType(repo.readScenarios(),true);
+
+    }
+
+    @Test
+    public void testModifyActionTypeById(){
+
+        modifyBALSuggestion.modifyActionTypeById(1,1, 1);
+        verify(repo).updateActionTypeById(1,1,1);
+        verify(out).showModifiedActionType(repo.readScenarios(),true);
+
+    }
+
+    @Test
+    public void testModifyActionName(){
+
+        modifyBALSuggestion.modifyActionName(1,1, "tipo");
+        verify(repo).updateActionName(1,1,"tipo");
+        verify(out).showModifiedActionName(repo.readScenarios(),true,"tipo");
+    }
+
+    @Test
+    public void testModifyObjectType(){
+
+        modifyBALSuggestion.modifyObjectType(1,1, 1,"tipo");
+        verify(repo).updateObjectType(1,1,1,"tipo");
+        verify(out).showModifiedObjectType(repo.readScenarios(),true);
+    }
+
+    @Test
+    public void testModifyObjectTypeById(){
+
+        modifyBALSuggestion.modifyObjectTypeById(1,1, 1,1);
+        verify(repo).updateObjectTypeById(1,1,1,1);
+        verify(out).showModifiedObjectType(repo.readScenarios(),true);
+    }
+
+    @Test
+    public void testModifyObjectName(){
+
+        modifyBALSuggestion.modifyObjectName(1,1, 1,"tipo");
+        verify(repo).updateObjectName(1,1,1,"tipo");
+        verify(out).showModifiedObjectName(repo.readScenarios(),true,"tipo");
+    }
+
+
+    @Test
+    public void testAddObject(){
+
+        modifyBALSuggestion.addObject(1,1, "tipo",1);
+        verify(repo).createObject(1,1,"tipo",1);
+        verify(out).showAddedObject(repo.readScenarios(),true);
+    }
+
+    @Test
+    public void testRemoveObject(){
+
+        modifyBALSuggestion.removeObject(1,1, 1);
+        verify(repo).deleteObject(1,1,1);
+        verify(out).showRemovedObject(repo.readScenarios(),true);
+    }
+
+
+
+}
